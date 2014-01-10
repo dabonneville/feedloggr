6a143e83a3d4a14e0050f01bee451201d666ce8a
lmas <lmasvensson@gmail.com>
2014-01-10 17:23:29 +0100

    Added some app commands.

--- example/manage.py
+++ example/manage.py
@@ -8,5 +8,16 @@ manager = Manager(app)
 from feedloggr.utils import manager as feedloggr_manager
 manager.add_command('feedloggr', feedloggr_manager)
 
+@manager.command
+def setup():
+    """Initiate databases."""
+    from app import setup
+    setup()
+
+@manager.command
+def routes():
+    """List all routes for this app."""
+    print(app.url_map)
+
 if __name__ == '__main__':
     manager.run()
