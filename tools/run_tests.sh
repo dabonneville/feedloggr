c5a61b0657df3118ea3e0cbbc3dd0b860672ece7
lmas <lmasvensson@gmail.com>
2014-01-18 15:56:50 +0100

    run_tests.sh now points to tests.py's new path

--- tools/run_tests.sh
+++ tools/run_tests.sh
@@ -2,7 +2,7 @@
 
 # The config file will be loaded relative from the instance folder
 export FEEDLOGGR_CONFIG=testing.py
-coverage run --source=feedloggr feedloggr/tests.py
+coverage run --source=feedloggr tests.py
 STATUS=$?
 unset FEEDLOGGR_CONFIG
 exit $STATUS
