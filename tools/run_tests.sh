cc9bfecc4a13c0dce5f913ccec5e3f9f2cde68fc
lmas <lmasvensson@gmail.com>
2014-01-18 15:00:00 +0100

    run_tests.sh now reports exit status correctly.

--- tools/run_tests.sh
+++ tools/run_tests.sh
@@ -2,7 +2,7 @@
 
 # The config file will be loaded relative from the instance folder
 export FEEDLOGGR_CONFIG=testing.py
-#python feedloggr/main.py test
 coverage run --source=feedloggr feedloggr/tests.py
+STATUS=$?
 unset FEEDLOGGR_CONFIG
-
+exit $STATUS
