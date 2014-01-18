021a34e933faf11d3d0be2761e560921420b306f
lmas <lmasvensson@gmail.com>
2014-01-18 14:47:02 +0100

    Now using coverage for tests.

--- tools/run_tests.sh
+++ tools/run_tests.sh
@@ -2,5 +2,7 @@
 
 # The config file will be loaded relative from the instance folder
 export FEEDLOGGR_CONFIG=testing.py
-python feedloggr/main.py test
+#python feedloggr/main.py test
+coverage run --source=feedloggr feedloggr/tests.py
 unset FEEDLOGGR_CONFIG
+
