841c2cc2d5a2570858c94b548cac856d50ea9e81
lmas <lmasvensson@gmail.com>
2014-01-20 19:14:18 +0100

    Refactored setup_blueprint and friends

--- feedloggr/blueprint/__init__.py
+++ feedloggr/blueprint/__init__.py
@@ -2,24 +2,16 @@
 """Collect news from your favorite RSS/Atom feeds and show them
 in your flask application."""
 
-from .views import blueprint
 from .models import Dates, Feeds, FeedsAdmin, Entries
 from .utils import create_tables
+from .views import blueprint
 
-def register_admin():
-    try:
-        from ..main import admin
-    except ImportError:
-        return False
-    else:
-        admin.register(Feeds, FeedsAdmin)
-        return True
-
-def setup_blueprint(app):
+def setup_blueprint(app, admin):
     try:
         url = app.config['FEEDLOGGR_URL']
     except KeyError:
         url = None
     app.register_blueprint(blueprint, url_prefix=url)
     create_tables(fail_silently=True)
-    register_admin()
+    admin.register(Feeds, FeedsAdmin)
+
