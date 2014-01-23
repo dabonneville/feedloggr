079fa7530f3c04034b269aa6c50c6899e26f32cc
lmas <lmasvensson@gmail.com>
2014-01-23 20:59:36 +0100

    Blueprint now behaves like a real extension.

--- feedloggr/blueprint/__init__.py
+++ feedloggr/blueprint/__init__.py
@@ -2,16 +2,37 @@
 """Collect news from your favorite RSS/Atom feeds and show them
 in your flask application."""
 
-from .models import Dates, Feeds, FeedsAdmin, Entries
+from flask import Blueprint
+
+from .models import db_proxy, Feeds, FeedsAdmin
+from .views import index
 from .utils import create_tables
-from .views import blueprint
 
-def setup_blueprint(app, admin):
-    try:
-        url = app.config['FEEDLOGGR_URL']
-    except KeyError:
-        url = None
-    app.register_blueprint(blueprint, url_prefix=url)
-    create_tables(fail_silently=True)
-    admin.register(Feeds, FeedsAdmin)
+class Feedloggr():
+    def __init__(self, app, db=None, admin=None):
+        self.app = app
+        if app is not None:
+            self.init_app(app, db, admin)
 
+    def init_app(self, app, db, admin):
+        blueprint = Blueprint(
+            'feedloggr',
+            __name__,
+            template_folder='templates',
+            static_folder='static',
+        )
+
+        blueprint.add_url_rule('/', view_func=index)
+        blueprint.add_url_rule(
+            '/<int:year>-<int:month>-<int:day>',
+            view_func=index,
+        )
+
+        db_proxy.initialize(db.database)
+        create_tables(fail_silently=True)
+        admin.register(Feeds, FeedsAdmin)
+
+        app.register_blueprint(
+            blueprint,
+            url_prefix = app.config.get('FEEDLOGGR_URL', '/feedloggr'),
+        )
