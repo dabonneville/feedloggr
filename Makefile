d6196dd71d395c13d2c318e6a6c3f298d4e8fc46
lmas <lmasvensson@gmail.com>
2014-02-08 19:03:15 +0100

    Can now build a package.

--- Makefile
+++ Makefile
@@ -8,7 +8,13 @@ test:
 	coverage run --source=$(PACKAGE_NAME) tests.py
 	rm -f test.db
 
+sdist:
+	python setup.py sdist
+
 clean:
+	rm -fr dist/
+	rm -fr $(PACKAGE_NAME).egg-info/
 	rm -f $(PACKAGE_NAME)/*.pyc
 	rm -f example/*.pyc
 	rm -f *.pyc
+
---
+++ setup.py
@@ -0,0 +1,26 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name='Feedloggr',
+    version='0.3',
+    description='Collect news from your favorite RSS/Atom feeds and show them in your flask application.',
+    long_description=open('README.md').read(),
+    url='http://github.org/lmas/Feedloggr',
+    author='A. Svensson',
+    author_email='lmasvensson at gmail dot com',
+    license='MIT',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'flask',
+        'flask',
+        'flask-script',
+        'flask-peewee',
+    ],
+    # List of classifiers:  https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Private :: Do Not Upload'
+    ],
+)
